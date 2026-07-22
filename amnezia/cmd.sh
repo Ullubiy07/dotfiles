fa480d0240e3d14a18b651a601f42bd968e67566
Ullubiy Saygitov <saygitov07@gmail.com>
2026-07-22 21:12:23 +0300

    Add commands for amnezia cli

---
+++ amnezia/cmd.sh
@@ -0,0 +1,27 @@
+# Узнаем версию ядра (пример: 6.18.38-1-MANJARO)
+uname -r
+
+# Устанавливаем заголовки для вашего ядра 6.18 и систему сборки DKMS
+sudo pacman -Sy linux618-headers dkms
+
+# Скачиваем и собираем сам VPN AmneziaWG из AUR
+yay -S amneziawg-dkms amneziawg-tools
+
+# Перезагружаем компьютер (это обязательно, чтобы ядро увидело новый драйвер)
+reboot
+
+# Проверка после перезагрузки
+dkms status
+
+# Создаем конфиг (комментируем DNS)
+sudo nano /etc/amnezia/amneziawg/awg0.conf
+
+# Включить VPN
+sudo awg-quick up awg0
+
+# Проверить, идет ли трафик и с какой скоростью
+sudo awg show
+
+# Выключить VPN (обычный интернет вернется сразу же)
+sudo awg-quick down awg0
+
