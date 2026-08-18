b4817bb9fd4739119be5222e6cdd2df9a6d5ca00
Ullubiy Saygitov <saygitov07@gmail.com>
2026-08-18 21:19:19 +0300

    Update zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,18 @@
-#!/bin/zsh
+#!/bin/bash
+
+mkdir -p /usr/share/zsh/plugins
+
+[ ! -d /usr/share/zsh/plugins/zsh-syntax-highlighting ] && \
+  git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting
+
+[ ! -d /usr/share/zsh/plugins/zsh-history-substring-search ] && \
+  git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search.git /usr/share/zsh/plugins/zsh-history-substring-search
+
+[ ! -d /usr/share/zsh/plugins/zsh-autosuggestions ] && \
+  git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh/plugins/zsh-autosuggestions
+
 mkdir -p ~/.config/zsh
 cp zsh.conf ~/.config/zsh/zsh.conf
 cp zshrc ~/.zshrc
+
+chsh -s $(which zsh)
